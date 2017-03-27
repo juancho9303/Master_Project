b7260d4589b7c354036aff45006e689f0c7bf80f
Juan Manuel Espejo Salcedo <juancho9303@dobbe.strw.leidenuniv.nl>
2017-03-27 20:40:19 +0200

    update

--- plots/Shear_vs_radius.py
+++ plots/Shear_vs_radius.py
@@ -6,26 +6,42 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 mpl.rcParams['text.usetex'] = True
 
-c        = 4.46           # concentration parameter (Paper Dutton and Maccio) 
-#delta_c  = 6716.38        # Dimensionless overdensity using 4.46
-delta_c = (200/3)*(c*c*c)/(np.log(1+c)-c/(1+c))
+c        = 4.46           # concentration parameter (Paper Dutton and Maccio)
+delta_c = (200./3)*(c**3)/(np.log(1+c)-c/(1+c))
 rho_c    = 197.8            # Critical density in M_sun/kpc^3 (197.8)
 r_s      = 100            # Characteristic radius (kpc) 
-sigma_c  = 10**8
+#sigma_c  = 4.76*10**8          # Critical surface mass density
+sigma_c  = 10**8          # Critical surface mass density for z=1
+sigma_c2 = 5.4*10**7      # Critical surface mass density for z=1.5     
+sigma_c3 = 2*10**7        # Critical surface mass density for z=2
 
 x1 = np.arange(0.001,0.99,0.0001)
-plt.plot(x1*r_s,((r_s*delta_c*rho_c+x1-x1)/(sigma_c))*( ((8*np.arctanh(np.sqrt((1-x1)/(1+x1))))/(x1*x1*np.sqrt(1-x1*x1))) + ((4*(np.log(x1/2)))/(x1*x1)) - ((2)/(x1*x1-1)) + ((4*np.arctanh(np.sqrt((1-x1)/(1+x1))))/((x1*x1-1)*(1-x1*x1)**0.5))))
+plt.plot(x1*r_s,((r_s*delta_c*rho_c)/(sigma_c))*( ((8.*np.arctanh(np.sqrt((1-x1)/(1+x1))))/(x1*x1*np.sqrt(1-x1*x1))) + ((4*(np.log(x1/2)))/(x1*x1)) - ((2)/(x1*x1-1)) + ((4*np.arctanh(np.sqrt((1-x1)/(1+x1))))/((x1*x1-1)*(1-x1*x1)**0.5))))
 x2 = 1
-plt.plot(x2*r_s,((r_s*delta_c*rho_c+x2-x2)/(sigma_c))*(10/3+4*np.log(0.5)))
+plt.plot(x2*r_s,((r_s*delta_c*rho_c)/(sigma_c))*(10./3.+4.*np.log(0.5)))
 x3 = np.arange(1.0001,10,0.0001)
-plt.plot(x3*r_s,((r_s*delta_c*rho_c+x3-x3)/(sigma_c))*( ((8*np.arctan(np.sqrt((x3-1)/(1+x3))))/(x3*x3*np.sqrt(x3*x3-1))) + ((4*(np.log(x3/2)))/(x3*x3)) - ((2)/(x3*x3-1)) + ((4*np.arctan(np.sqrt((x3-1)/(1+x3))))/((x3*x3-1)**1.5))))
+plt.plot(x3*r_s,((r_s*delta_c*rho_c)/(sigma_c))*( ((8.*np.arctan(np.sqrt((x3-1)/(1+x3))))/(x3*x3*np.sqrt(x3*x3-1))) + ((4*(np.log(x3/2)))/(x3*x3)) - ((2)/(x3*x3-1)) + ((4*np.arctan(np.sqrt((x3-1)/(1+x3))))/((x3*x3-1)**1.5))), label='z=1')
+
+x4 = np.arange(0.001,0.99,0.0001)
+plt.plot(x4*r_s,((r_s*delta_c*rho_c)/(sigma_c2))*( ((8.*np.arctanh(np.sqrt((1-x4)/(1+x4))))/(x4*x4*np.sqrt(1-x4*x4))) + ((4*(np.log(x4/2)))/(x4*x4)) - ((2)/(x4*x4-1)) + ((4*np.arctanh(np.sqrt((1-x4)/(1+x4))))/((x4*x4-1)*(1-x4*x4)**0.5))))
+x5 = np.arange(1.0001,10,0.0001)
+plt.plot(x5*r_s,((r_s*delta_c*rho_c)/(sigma_c2))*( ((8.*np.arctan(np.sqrt((x5-1)/(1+x5))))/(x5*x5*np.sqrt(x5*x5-1))) + ((4*(np.log(x5/2)))/(x5*x5)) - ((2)/(x5*x5-1)) + ((4*np.arctan(np.sqrt((x5-1)/(1+x5))))/((x5*x5-1)**1.5))), label='z=1.5')
+
+x6 = np.arange(0.001,0.99,0.0001)
+plt.plot(x6*r_s,((r_s*delta_c*rho_c)/(sigma_c3))*( ((8.*np.arctanh(np.sqrt((1-x6)/(1+x6))))/(x6*x6*np.sqrt(1-x6*x6))) + ((4*(np.log(x6/2)))/(x6*x6)) - ((2)/(x6*x6-1)) + ((4*np.arctanh(np.sqrt((1-x6)/(1+x6))))/((x6*x6-1)*(1-x6*x6)**0.5))))
+x7 = np.arange(1.0001,10,0.0001)
+plt.plot(x7*r_s,((r_s*delta_c*rho_c)/(sigma_c3))*( ((8.*np.arctan(np.sqrt((x7-1)/(1+x7))))/(x7*x7*np.sqrt(x7*x7-1))) + ((4*(np.log(x7/2)))/(x7*x7)) - ((2)/(x7*x7-1)) + ((4*np.arctan(np.sqrt((x7-1)/(1+x7))))/((x7*x7-1)**1.5))), label='z=2')
+
+plt.axhline(y=.5, xmin=0., xmax=10., linewidth=1, color = 'k')
 
 #plt.yscale('log')
 #plt.xscale('log')
+#plt.axis((y1,y2,0,3))
+plt.ylim(0,3)
 plt.xlabel('$R(kpc)$')
 plt.ylabel('$\gamma_{NFW}$')
 plt.title('Shear dependence on radius')
 plt.grid(True)
-#plt.legend()
+plt.legend()
 plt.savefig("Shear dependence on radius.png")
 plt.show()
--- plots/Surface_mass_density.py
+++ plots/Surface_mass_density.py
@@ -9,57 +9,56 @@ mpl.rcParams['text.usetex'] = True
 # This plot is made for the cluster ABELL 1068
 
 c        = 4.46           # concentration parameter (Paper Dutton and Maccio) this was checked many times 
-#delta_c  = 6716.38        # Dimensionless overdensity using 4.46
-delta_c = (200/3)*(c*c*c)/(np.log(1+c)-c/(1+c))
-rho_c    = 200            # Critical density in M_sun/kpc^3 (197.8)
-r_s      = 100            # Characteristic radius (kpc) 
-I_e      = 2.6*10**9      # Effective brightness I(Re) 2.6e6
+delta_c = (200./3)*(c**3)/(np.log(1+c)-c/(1+c))
+rho_c    = 197.8            # Critical density in M_sun/kpc^3 (197.8)
+r_s      = 100.            # Characteristic radius (kpc) 
+I_e      = 5.1*10**8     # Effective brightness I(Re) 2.6e6
 b        = 7.67           # Lokas and Mamon paper
-R_e      = 20             # Half of the total light in kpc
+R_e      = 73.7             # Half of the total light in kpc
 
 # Mass density profile from NFW density profile:
 
-x1 = np.arange(0.001,1,0.0001)
-plt.plot(x1*r_s,((2*r_s*delta_c*rho_c)/((x1*x1)-1))*(1-(2)/(np.sqrt(1-(x1*x1)))*np.arctanh(np.sqrt((1-x1)/(1+x1)))))
+x1 = np.arange(0.001,1.,0.0001)
+plt.plot(np.log10(x1*r_s),np.log10(((2*r_s*delta_c*rho_c)/((x1*x1)-1))*( 1. - 2./(np.sqrt(1-(x1*x1)))*np.arctanh(np.sqrt((1-x1)/(1+x1))))))
 x2 = 1
-plt.plot(x2*r_s,(2*r_s*delta_c*rho_c)/3)
-x3 = np.arange(1.0001,100,0.0001)
-plt.plot(x3*r_s,((2*r_s*delta_c*rho_c)/((x3*x3)-1))*(1-(2)/(np.sqrt((x3*x3)-1))*np.arctan(np.sqrt((x3-1)/(1+x3)))),label='NFW profile')
+plt.plot(np.log10(x2*r_s),np.log10((2*r_s*delta_c*rho_c)/3.))
+x3 = np.arange(1.0001,100.,0.0001)
+plt.plot(np.log10(x3*r_s),np.log10(((2*r_s*delta_c*rho_c)/((x3*x3)-1))*(1. - 2./(np.sqrt((x3*x3)-1))*np.arctan(np.sqrt((x3-1)/(1+x3))))),label='NFW profile')
 
 # Mass density profile from M/L ratio and de Vaucouleurs profile:
 
-R = np.arange(0.1,10000,0.001)
-plt.plot(R,4*I_e*np.exp(-b*(((R)/(R_e))**(0.25)-1)),label='Stellar Surface Mass')  #4*I(R) where I(R) is de Vaucouleurs
+R = np.arange(0.1,10000.,0.001)
+plt.plot(np.log10(R),np.log10(4.*I_e*np.exp( -b*( ( R/R_e )**0.25 - 1. ) )),label='Stellar Surface Mass')  #4*I(R) where I(R) is de Vaucouleurs
 
 # INTEGRATIONS:
 
 # For the total luminosity:
 
-def surf_bright(r, I_e, b, R_e):
-	return 4*I_e*np.exp(-b*(((r)/(R_e))**(0.25)-1))
-I1, I1err = quad(surf_bright, 0.1, 10000, args=(I_e,b,R_e))
+def surf_bright(r):
+	return 2.*np.pi*r*I_e*np.exp( -b*( ( r/R_e )**0.25 - 1. ) )
+I1, I1err = quad(surf_bright, 0.001, 10000.)
 #r = np.arange(0.1,10,0.001)
 
 print 'Luminosity: %e' %(I1)
 
 # Projected masses found by integrating the projected surface mass density
 
-def NFW1(x1, delta_c, r_s, rho_c):
-	return 2*np.pi*x1*((2*r_s*delta_c*rho_c)/((x1*x1)-1))*(1-(2)/(np.sqrt(1-(x1*x1)))*np.arctanh(np.sqrt((1-x1)/(1+x1))))
-I2, I2err = quad(NFW1, 0.001, 1, args=(delta_c, r_s, rho_c))
+def NFW1(x):
+	return 2*np.pi*x*((2.*r_s*delta_c*rho_c)/((x*x)-1))*(1-2/(np.sqrt(1-(x*x)))*np.arctanh(np.sqrt((1-x)/(1+x))))
+I2, I2err = quad(NFW1, 0.001, 1.)
 
-def NFW2(x3, delta_c, r_s, rho_c):
-	return 2*np.pi*x3*((2*r_s*delta_c*rho_c)/((x3*x3)-1))*(1-(2)/(np.sqrt((x3*x3)-1))*np.arctan(np.sqrt((x3-1)/(1+x3))))
-I3, I3err = quad(NFW2, 1.001, 100, args=(delta_c, r_s, rho_c))
+def NFW2(x):
+	return 2*np.pi*x*((2*r_s*delta_c*rho_c)/((x*x)-1))*(1-2/(np.sqrt((x*x)-1))*np.arctan(np.sqrt((x-1)/(1+x))))
+I3, I3err = quad(NFW2, 1.001, 100.)
 IT = I2+I3
 print 'Dark Matter: %e' %(IT)
 
-plt.yscale('log')
-plt.xscale('log')
-plt.xlabel('$R(kpc)$')
-plt.ylabel('$\Sigma_{NFW}(M_{\odot }/ kpc^{2})$')
+#plt.yscale('log')
+#plt.xscale('log')
+plt.xlabel('$\log R(kpc)$')
+plt.ylabel('$\log \Sigma_{NFW}(M_{\odot }/ kpc^{2})$')
 plt.title('Surface Mass Density')
 plt.grid(True)
 plt.legend()
-plt.savefig("Surface_mass_density_full.png")
+plt.savefig("Surface_mass_density_log.png")
 plt.show()
