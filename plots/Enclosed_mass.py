e7331e8011d9b5208bfebdcc6ecd0ad8471ff295
Juan Manuel Espejo Salcedo <juancho9303@dobbe.strw.leidenuniv.nl>
2017-03-29 17:35:09 +0200

    update

--- plots/Enclosed_mass.py
+++ plots/Enclosed_mass.py
@@ -1,60 +1,55 @@
 import numpy as np
 from scipy import integrate
-from scipy.integrate import quad
 import matplotlib as mpl
-import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 mpl.rcParams['text.usetex'] = True
 
-c        = 4.46                                   # concentration parameter (Paper Dutton and Maccio) this was checked many times 
-delta_c  = (200./3)*(c**3)/(np.log(1+c)-c/(1+c))
-rho_c    = 197.8                                  # Critical density in M_sun/kpc^3 (197.8)
-r_s      = 100                                    # Characteristic radius (kpc) 
-I_e      = 5.1*10**8                              # Effective brightness I(Re) 2.6e6
-b        = 7.67                                   # Lokas and Mamon paper
-R_e      = 73.7                                   # Half of the total light in kpc
+c        = 4.46                                      # Concentration parameter (Paper Dutton and Maccio) 
+delta_c  = ((200./3)*c**3)/(np.log(1.0+c)-c/(1.0+c)) # Characteristic overdensity of the halo
+rho_c    = 197.8                                     # Critical density in M_sun/kpc^3 (197.8)
+r_s      = 100                                       # Characteristic radius (kpc) 
+I_e      = 10**7                                     # Effective brightness I(Re)
+b        = 7.67                                      # Lokas and Mamon paper
+R_e      = 73.7                                      # Half of the total light in kpc
+n_array  = [] 
+f_array  = []
+R        = np.arange(0.01, 10000, 10.)
 
-#MASS ENCLOSED BY LIGHT (DE VAUCOULEURS PROFILE)
+# MASS ENCLOSED BY LIGHT (DE VAUCOULEURS PROFILE)
 
 def surf_bright(r):
-	return 2*np.pi*I_e*np.exp(-b*((r/(R_e))**(0.25)-1))
-R = np.arange(0.1,10000,0.1)
-def F(r):
-    res = np.zeros_like(r)
-    for i,val in enumerate(r):
-        y,err = integrate.quad(surf_bright,0,val)
-        res[i]=y
-    return res
-plt.plot(R,F(R),label='Mass in Stars')
+	return 8.0*np.pi*I_e*np.exp( -b*( ( r/R_e )**0.25 - 1.0 ) )
 
-#NOW FOR THE MASS OF THE NFW PROFILE
+# NOW FOR THE MASS OF THE NFW PROFILE
 
-def NFW(x):
-	if (x<1.0):
-		surf_mass = 2.*np.pi*x*((2*r_s*delta_c*rho_c)/((x*x)-1))*(1-(2)/(np.sqrt(1-(x*x)))*np.arctanh(np.sqrt((1-x)/(1+x))))
-	elif (x==1.0):
-		surf_mass = (2.*r_s*delta_c*rho_c)/3
+def NFW(r):
+	if (r < r_s):
+		surf_mass = 2.0*np.pi*r/r_s*( ( 2.0*r_s*delta_c*rho_c )/( (r/r_s)**2 - 1.0 ) )*( 1.0 - 2.0/( np.sqrt( 1.0 - (r/r_s)**2 ) )*np.arctanh( np.sqrt( ( 1.0-r/r_s )/( 1.0 + r/r_s ) ) ) )
+	elif (r == r_s):
+		surf_mass = 2.0*r_s*delta_c*rho_c/3.0
 	else:
-		surf_mass = 2.*np.pi*x*((2*r_s*delta_c*rho_c)/((x*x)-1))*(1-(2)/(np.sqrt((x*x)-1))*np.arctan(np.sqrt((x-1)/(1+x))))
+		surf_mass = 2.0*np.pi*r/r_s*( ( 2.0*r_s*delta_c*rho_c )/( (r/r_s)**2 - 1.0 ) )*( 1.0 - 2.0/( np.sqrt( (r/r_s)**2 - 1.0 ) )*np.arctan( np.sqrt( ( r/r_s - 1.0 )/( 1 + r/r_s ) ) ) )
 	return surf_mass
 	
-X = np.arange(0.001,100,0.1)
+def F(val):
+  h = lambda x: surf_bright(x)
+  return integrate.quad(h, 0.1, val)[0]
 
-def Mass(x):
-    res2 = np.zeros_like(x)
-    for j,val in enumerate(x):
-        y,err = integrate.quad(NFW,0,val)
-        res2[j]=y
-    return res2
-plt.plot(X*r_s,Mass(X),label='Dynamical Mass')
-
-plt.yscale('log')
-plt.xscale('log')
-plt.xlabel('$R(kpc)$')
-plt.ylabel('$M_{\odot }$')
-plt.title('Enclosed Mass')
+def N(val):
+  g = lambda y: NFW(y)
+  return integrate.quad(g, 0.1, val)[0]
+      
+for i in range(len(R)):
+   f_array.append(np.log10(F(R[i])))
+   n_array.append(np.log10(N(R[i])))
+    
+plt.plot(np.log10(R), f_array, '-', c = 'r', label = r'$\mathrm{Stellar\:Mass}$')
+plt.plot(np.log10(R), n_array, '-', c = 'b', label = r'$\mathrm{NFW\:Profile\:Mass}$')
+plt.xlabel(r'$\log R(kpc)$',fontsize=18)
+plt.ylabel(r'$\log M_{\odot }$',fontsize=18)
+plt.title(r'$\mathrm{Enclosed\:Mass}$',fontsize=18)
 plt.grid(True)
-plt.legend()
+plt.legend(bbox_to_anchor=(0.95, 0.2), loc=1, borderaxespad=0.)
 plt.savefig("Enclosed Mass.png")
 plt.show()
-
+plt.show()      
