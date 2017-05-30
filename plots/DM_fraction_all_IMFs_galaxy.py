9973f386f0b480a2667b753172c2a326abae9ac9
Juan Espejo <juancho9303@gmail.com>
2017-05-30 12:48:53 +0200

    update

--- plots/DM_fraction_all_IMFs_galaxy.py
+++ plots/DM_fraction_all_IMFs_galaxy.py
@@ -7,16 +7,16 @@ from matplotlib import gridspec
 mpl.rcParams['text.usetex'] = True
 from operator import truediv
 
-c          = 4.46                                      # Concentration parameter (Paper Dutton and Maccio) 
+c          = 7.9428                                      # Concentration parameter (Paper Dutton and Maccio) 
 delta_c    = ((200./3)*c**3)/(np.log(1.0+c)-c/(1.0+c)) # Characteristic overdensity of the halo
 rho_c      = 212                                       # Critical density in M_sun/kpc^3 (212)
-r_s        = 307.1                                     # Characteristic radius (kpc) calculated using M200 of 9.8e15 
+r_s        = 25.2                                    # Characteristic radius (kpc) calculated using M200 of 9.8e15 
 I_e        = 10**7                                     # Effective brightness I(Re)
 b          = 7.67                                      # Lokas and Mamon paper
-R_e        = 73.7                                      # Half of the total light in kpc
+R_e        = 17                                      # Half of the total light in kpc
 n_array     = []; n_array_plus  = []; n_array_minus   = []; f_array1    = []
 fractionDM1 = []; f_array2      = []; fractionDM2     = []; f_array3    = []; fractionDM3 = []
-R           = np.arange(0.0001, 100, 0.1)
+R           = np.arange(0.0001, 10, 0.1)
 
 # MASS ENCLOSED BY LIGHT (SALPETER)
 def surf_bright1(r):
@@ -81,20 +81,19 @@ fig = plt.figure()
 gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1]) 
 
 ax0 = plt.subplot(gs[0])
-ax0.set_xlim([0.1, 100])
+ax0.set_xlim([0.1, 10])
 line0, = ax0.loglog((R), (f_array1), '-.', c='darkorange', label = r'$\mathrm{Salpeter}$', lw = 2)
 line0, = ax0.loglog((R), (f_array2), '--', c='darkred', label = r'$\mathrm{Kroupa}$', lw = 2)
 line0, = ax0.loglog((R), (f_array3), '-.', c='darkgreen', label = r'$\mathrm{Chabrier}$', lw = 2)
 line0, = ax0.loglog((R), (n_array), '-', c='black', label = r'$\mathrm{NFW\:Mass\:Profile}$', lw = 2)
+#plt.fill_between(R, 0.3*n_array, 0.7*n_array, alpha=0.2)
 plt.fill_between(R, n_array_minus, n_array_plus, alpha=0.2)
 plt.ylabel(r'$\mathrm{M(r)/M_{\odot}}$',fontsize=18)
 plt.legend(frameon=False,bbox_to_anchor=(0.95, 0.45), loc=1, borderaxespad=0.,prop={'size':13})
 
 ax1 = plt.subplot(gs[1], sharex = ax0)
-ax0.set_xlim([0.1, 100])
-#plt.xscale('log')
-#plt.yscale('log')
-line1, = ax1.loglog((R), (fractionDM1), '-.', c='darkorange', label = r'$\mathrm{M_{NFW}/M_{\star}\:Salpeter}$', lw = 2)
+ax0.set_xlim([0.1, 10])
+line1, = ax1.plot((R), (fractionDM1), '-.', c='darkorange', label = r'$\mathrm{M_{NFW}/M_{\star}\:Salpeter}$', lw = 2)
 line1, = ax1.plot((R), (fractionDM2), '--', c='darkred', label = r'$\mathrm{M_{NFW}/M_{\star}\:Kroupa}$', lw = 2)
 line1, = ax1.plot((R), (fractionDM3), ':', c='darkgreen', label = r'$\mathrm{M_{NFW}/M_{\star}\:Chabrier}$', lw = 2)
 plt.xlabel(r'$\mathrm{r(kpc)}$',fontsize=18)
@@ -108,5 +107,5 @@ yticks[-2].label1.set_visible(False)
 plt.axhline(1.0, c = 'black', linestyle='--')
 
 plt.subplots_adjust(hspace=.0)
-plt.savefig("DM_fraction_all_IMFs_try.png")
+plt.savefig("DM_fraction_all_IMFs_galaxy.png")
 plt.show()
