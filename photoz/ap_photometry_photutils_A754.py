1560f6c48c678d421cb73acb87a65b4f55fea9fc
Juan Manuel Espejo Salcedo <juancho9303@dobbe.strw.leidenuniv.nl>
2017-06-13 17:58:41 +0200

    update

--- photoz/ap_photometry_photutils_A754.py
+++ photoz/ap_photometry_photutils_A754.py
@@ -3,20 +3,23 @@ from photutils import aperture_photometry,CircularAperture
 from photutils import CircularAnnulus
 from astropy.io import fits
 from astropy.table import Table
+import matplotlib.pylab as plt
 
 A1=fits.getdata('A754gready.fits') 
-B=Table.read('filtered_flux_g.dat',format='ascii') 
 A2=fits.getdata('A754rready.fits') 
 A3=fits.getdata('A754iready.fits') 
 A4=fits.getdata('A754uready.fits') 
+B=Table.read('filtered_flux_g.dat',format='ascii') 
 
+#The background in a smooth region
+background_coords = [(30., 30.)]
+apertures2 = CircularAperture(background_coords, r=3.2)
+#print(apertures2)
+
+#The objects found by sextractor
 positions = [B['col4'], B['col5']]
 apertures = CircularAperture(positions, r=3.2)
 
-positions2 = [(30., 30.)]
-apertures2 = CircularAperture(positions2, r=3.)
-
-#apertures = CircularAperture(positions, r=3.2)
 annulus_apertures = CircularAnnulus(positions, r_in=6., r_out=7.5)
 
 apers = [apertures, annulus_apertures]
@@ -30,29 +33,24 @@ bkg_mean = phot_tabler['aperture_sum_1'] / annulus_apertures.area()
 bkg_mean = phot_tablei['aperture_sum_1'] / annulus_apertures.area()
 bkg_mean = phot_tableu['aperture_sum_1'] / annulus_apertures.area()
 
-bkg_sum = bkg_mean * apertures.area()
+bkg_sum = ((bkg_mean+1.)/2.) * apertures.area()
 
 final_sum = phot_tableg['aperture_sum_0'] - bkg_sum
 phot_tableg['residual_aperture_sum'] = final_sum
 print(phot_tableg) 
-#print(phot_tableg['residual_aperture_sum'])    
+#np.savetxt('ap_phot_A754g.dat', phot_tableg, fmt='%f')   
 
 final_sum = phot_tabler['aperture_sum_0'] - bkg_sum
 phot_tabler['residual_aperture_sum'] = final_sum
-print(phot_tabler) 
-#print(phot_tabler['residual_aperture_sum'])    
+print(phot_tabler)     
 
 final_sum = phot_tablei['aperture_sum_0'] - bkg_sum
 phot_tablei['residual_aperture_sum'] = final_sum
-print(phot_tablei) 
-#print(phot_tablei['residual_aperture_sum'])    
+print(phot_tablei)     
 
 final_sum = phot_tableu['aperture_sum_0'] - bkg_sum
 phot_tableu['residual_aperture_sum'] = final_sum
-print(phot_tableu) 
-#print(phot_tableu['residual_aperture_sum'])    
-
-import matplotlib.pylab as plt
+print(phot_tableu)    
 
 plt.imshow(A1,interpolation='None',origin='lower',vmin=-1e3)
 apertures.plot()
